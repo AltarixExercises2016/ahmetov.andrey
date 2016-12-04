5da6d3dafc31dc0191183bd2ad3e48e0a3391fd9
Andrey092009 <andrey092009@gmail.com>
2016-12-05 00:36:20 +0400

    HomeWork4 add synchronized

--- HomeWork4.java
+++ HomeWork4.java
@@ -6,24 +6,25 @@ import java.util.Random;
  */
 public class HomeWork4 {
 
-    static ArrayList<String> ListWork = new ArrayList<>();
+    static final ArrayList<String>  ListWork = new ArrayList<>();
+
 
     public static void main(String[] args) {
 
         Thread WriteThread = new Thread(new Runnable() {
             @Override
             public void run() {
-
                 int bar = 0;
                 int[] timeArray = {7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
                 Random randTime = new Random();
                 long timeRead = System.currentTimeMillis();
                 long diff = 0;
                 while (true) {
-//                    System.out.println("Пищущий поток "+timeArray[bar]);
-                    bar = randTime.nextInt(11);
-                    diff = System.currentTimeMillis() - timeRead;
-                    ListWork.add(timeArray[bar] + "  " + diff);
+                    synchronized (ListWork) {
+                        bar = randTime.nextInt(11);
+                        diff = System.currentTimeMillis() - timeRead;
+                        ListWork.add(timeArray[bar] + "  " + diff);
+                    }
                     try {
                         Thread.sleep(timeArray[bar] * 100);
 
@@ -37,21 +38,22 @@ public class HomeWork4 {
         Thread ReadThread = new Thread(new Runnable() {
             @Override
             public void run() {
-
                 long timeRead = System.currentTimeMillis();
                 long diff = 0;
                 while (true) {
                     diff = System.currentTimeMillis() - timeRead;
-                    try {
-                        Thread.sleep(1000);
-                        System.out.println(ListWork.get(0) + "  " + ListWork.size() + "  " + diff);
-                        ListWork.remove(0);
+                        try {
+                            Thread.sleep(1000);
+                            synchronized (ListWork) {
+                                System.out.println(ListWork.get(0) + "  " + ListWork.size() + "  " + diff);
+                                ListWork.remove(0);
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("Список пуст");
+                        }
 
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (IndexOutOfBoundsException e) {
-                        System.out.println("Список пуст");
-                    }
                 }
             }
         });
